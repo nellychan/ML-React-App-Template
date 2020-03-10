8f8435e48643904fb6413e1f0a978ffc0d74f2f6
nellychan <56140765+nellychan@users.noreply.github.com>
2020-03-10 21:29:33 +1100

    Update model_generator.py

--- showcase/ML-React-App-Template/service/model_generator.py
+++ showcase/ML-React-App-Template/service/model_generator.py
@@ -1,35 +1,44 @@
 # Import libraries
 import numpy as np
 import pandas as pd
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.externals import joblib
-from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 # Get the dataset
-dataset = datasets.load_iris()
+flight_data = pd.read_csv('flight_data.csv',sep=',')
+
+#Subset data
+flight_df1=flight_data[['DAY_OF_WEEK','MONTH','BusyFactor','Hourofdeparture_converted','Delay15Min']]
 
 # Split the dataset into features and labels
-X = dataset.data
-y = dataset.target
+data_1 = flight_df1.drop('Delay15Min',1) #Feature Matrix
+target_1 = flight_df1['Delay15Min'] #Target Variable
 
-# Split the dataset into training (80%) and testing (20%) data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0, shuffle = True)
+#Features are already encoded
 
+#Scaling encoded features
+data_1_df = data_1.copy()
+Data_scaler = preprocessing.MinMaxScaler()
+Data_scaler.fit(data_1)
+data_1 = Data_scaler.fit_transform(data_1)
+
+# Split the dataset into training (70%) and testing (30%) data
+data_1_train, data_1_test, \
+target_1_train, target_1_test = train_test_split(data_1, target_1, 
+                                                    test_size = 0.3, random_state=999,
+                                                    stratify = target_1)
 # Build the classifier and make prediction
 classifier = DecisionTreeClassifier()
-classifier.fit(X_train, y_train)
-prediction = classifier.predict(X_test)
+classifier.fit(data_1_train, target_1_train)
+prediction = classifier.predict(data_1_test)
 
 # Print the confusion matrix
 print("Confusion Matrix:")
-print(confusion_matrix(y_test, prediction))
+print(confusion_matrix(target_1_test, prediction))
 
 # Save the model to disk
 joblib.dump(classifier, 'classifier.joblib')
-
-
-
-
-
